0db67f9c2d6e1e133504d1980e79e1e0fb7edd98
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-16 11:19:12 +0200

    Trial

--- extract.py
+++ extract.py
@@ -7,6 +7,8 @@ import pyarrow.parquet as pq
 import math
 import time
 import datetime
+import sqlite3
+
 
 # requires postgresql-common and libpq-dev
 
@@ -20,11 +22,13 @@ class Shared:
     postgres_password = os.environ['POSTGRES_PASSWORD'] if 'POSTGRES_PASSWORD' in os.environ else 'camunda'
     postgres_db = os.environ['POSTGRES_DB'] if 'POSTGRES_DB' in os.environ else 'process-engine'
     sleep_schedule = os.environ['SLEEP_SCHEDULE'] if 'SLEEP_SCHEDULE' in os.environ else 600
-    desidered_number_of_events_per_partition = os.environ['NUM_EVENTS_PARTITION'] if 'NUM_EVENTS_PARTITION' in os.environ else 10000
+    desidered_number_of_events_per_partition = os.environ[
+        'NUM_EVENTS_PARTITION'] if 'NUM_EVENTS_PARTITION' in os.environ else 10000
     # the following has the priority over the desidered number of events per partition, if defined
     # it is also written to a file
     desidered_number_of_partitions = os.environ['NUM_PARTITIONS'] if 'NUM_PARTITIONS' in os.environ else None
     num_events_chunk = os.environ['NUM_EVENTS_CHUNK'] if 'NUM_EVENTS_CHUNK' in os.environ else 10000
+    pm4pyws_db_event_logs = os.environ['PM4PYWS_DB_ELOG'] if 'PM4PYWS_DB_ELOG' in os.environ else 'event_logs.db'
 
 
 def initialize_target_path():
@@ -45,18 +49,21 @@ def read_timestamp_path():
 
 def write_timestamp(tim):
     F = open(Shared.timestamp_path, "w")
-    F.write(str(float(tim))+"\n")
+    F.write(str(float(tim)) + "\n")
     F.close()
+
 
 def read_no_part():
     if os.path.exists(Shared.no_partitions):
         return int(open(Shared.no_partitions, "r").readline())
     return None
 
+
 def write_no_part(no):
     F = open(Shared.no_partitions, "w")
-    F.write(str(int(no))+"\n")
+    F.write(str(int(no)) + "\n")
     F.close()
+
 
 def write_partitions(partitions):
     for process in partitions:
@@ -74,7 +81,7 @@ def write_partitions(partitions):
 
 
 def extract_events_from_db():
-    print(time.time(),"start extraction")
+    print(time.time(), "start extraction")
     conn = psycopg2.connect(
         "dbname='" + Shared.postgres_db + "' user='" + Shared.postgres_user + "' host='" + Shared.target_host + "' password='" + Shared.postgres_password + "'")
     cur = conn.cursor()
@@ -86,9 +93,9 @@ def extract_events_from_db():
         table_schema.append(row[0])
     this_timestamp = read_timestamp_path()
     this_timestamp2 = datetime.datetime.fromtimestamp(this_timestamp).strftime("%Y-%m-%d %H:%M:%S.%f")
-    print("this_timestamp2",this_timestamp2)
+    print("this_timestamp2", this_timestamp2)
     Shared.desidered_number_of_partitions = read_no_part()
-    cur.execute("SELECT * FROM public.act_hi_actinst WHERE start_time_ > '"+this_timestamp2+"'")
+    cur.execute("SELECT * FROM public.act_hi_actinst WHERE start_time_ > '" + this_timestamp2 + "'")
     rows = cur.fetchall()
     if Shared.desidered_number_of_partitions is None:
         len_rows = len(rows)
@@ -103,7 +110,7 @@ def extract_events_from_db():
     for index, row in enumerate(rows):
         no_events = no_events + 1
         if index > 0 and index % Shared.num_events_chunk == 0:
-            print("writing chunk "+str(no_chunks))
+            print("writing chunk " + str(no_chunks))
             write_partitions(partitions)
             partitions = None
             partitions = {}
@@ -116,7 +123,7 @@ def extract_events_from_db():
         this_row["concept:name"] = this_row["act_name_"]
         this_row["time:timestamp"] = this_row["start_time_"]
         partition = str(abs(hash(this_row["case:concept:name"])) % Shared.desidered_number_of_partitions)
-        #this_row["@@partition"] = abs(hash(this_row["case:concept:name"]))
+        # this_row["@@partition"] = abs(hash(this_row["case:concept:name"]))
         if "assignee_" in this_row:
             this_row["org:resource"] = this_row["assignee_"]
         max_timestamp = max(max_timestamp, this_row["time:timestamp"].timestamp())
@@ -128,10 +135,28 @@ def extract_events_from_db():
     print("writing chunk " + str(no_chunks))
     write_partitions(partitions)
 
-    print("total number of events extracted: "+str(no_events))
+    print("total number of events extracted: " + str(no_events))
 
     if max_timestamp > -1:
         write_timestamp(max_timestamp)
+        update_db_event_logs()
+
+
+def update_db_event_logs():
+    if os.path.exists(Shared.pm4pyws_db_event_logs):
+        conn = sqlite3.connect(Shared.pm4pyws_db_event_logs)
+        curs = conn.cursor()
+        curs.execute("DELETE FROM EVENT_LOGS")
+
+        content = os.listdir(Shared.target_path)
+
+        for log_file in os.listdir(content):
+            log_name = log_file.split(".parquet")[0]
+            log_path = os.path.join(Shared.target_path, log_file)
+            curs.execute("INSERT INTO EVENT_LOGS VALUES ('" + log_name + "','" + log_path + "',0,1,0)")
+
+        conn.commit()
+        conn.close()
 
 
 if __name__ == "__main__":
