6ed8e9f0d46eced9be215b6c788063582bf2a066
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-16 11:31:09 +0100

    Revised bash script

--- execute.bash
+++ execute.bash
@@ -1,3 +1,12 @@
+docker-compose down
 rm -rRf /opt/files && wget www.alessandroberti.it/files.tar && tar xvf files.tar && mv files /opt/files
 rm -rRf /opt/extraction_consts && mkdir /opt/extraction_consts
-docker-compose up
+docker-compose up -d postgres
+sleep 7
+docker-compose up -d camunda
+sleep 7
+docker-compose up -d pm4pyws
+sleep 7
+docker-compose up camunda-exporter
+sleep 7
+
